37fe3c30632b0e737e88514129b26b1d2226ac42
EtharAlali <ethar@dynacognetics.com>
2016-02-05 14:44:06 +0000

    We refactored the Add , Subtract function to remove the reliance of the Current or accumulator. We also added Modulus and Average function.

--- src/calculator.js
+++ src/calculator.js
@@ -1,35 +1,50 @@
 //JavaScript Code for a simple Addition and Subtraction
 
- window.Calculator = {
-    
+window.Calculator = {
     current: 0,
 
-   //Addition  Function
-    add: function (  ){
-   
-       var sum =  this.current;
+    //Addition  Function
+    add: function() {
 
-       for( var i =0, len = arguments.length; i < len; i++ ) {
-       	  sum = sum + arguments[i];
-       }
-    	this.current = sum;
-    	return sum;
+        var sum = 0;
+
+        for (var i = 0, len = arguments.length; i < len; i++) {
+            sum = sum + arguments[i];
+        }
+        return sum;
     },
 
-   //Subtracting Function
-    subtract: function (  ){
-   
-       var result =  this.current;
+    //Subtracting Function
+    subtract: function() {
 
-       for( var i =0, len = arguments.length; i < len; i++ ) {
-       	  result = result - arguments[i];
-       }
-    	this.current = result;
-    	return result;
+        var result = arguments[0];
+
+        for (var i = 1; i < arguments.length; i++) {
+            result -= arguments[i];
+
+        }
+
+        return result;
+    },
+
+    multiply: function() {
+        this.current = (arguments[0] * arguments[1]);
+
+        return this.current;
     },
     //The Reset Function
-     reset: function () {
-       this.current = 0;
-     }
+    reset: function() {
+        this.current = 0;
+    },
 
+    divide: function(x,y) {
+        return x/y;
+    },
+    modulus: function (x, y) {
+        return x % y;
+    },
+    average: function(x,y) {
+        return (x + y) / 2;
+
+    },
  };
